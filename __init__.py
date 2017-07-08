73f97b0c07298824f4d5452b716b8e0be5d34d8f
John Richmond <confusedjohn46@gmail.com>
2017-07-08 13:14:01 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 20:36:45 2017
+
+@author: johnrichmond
+"""
+
+
+from .dataset import Dataset, TensorDataset, ConcatDataset
+from .dataloader import DataLoader
+from .jrTest import *
